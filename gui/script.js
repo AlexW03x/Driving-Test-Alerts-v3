a27d1ddbe5ca4556a1514e796633b3fb32374dd9
Alex Walker <106552874+AlexW03x@users.noreply.github.com>
2025-01-16 01:47:36 +0000

    working settings save and load

--- gui/script.js
+++ gui/script.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("tab1").classList.add("bg-[#5c9c40]");
+
+    eel.loadScriptSettings("hidden");
 })
 
 let tab = 1;
@@ -42,4 +44,137 @@ function changeTabs(tabs){
         console.log("incorrect tab value!");
     }
     tab = tabs;
+}
+
+let drivingLicense = "";
+let postcode = "";
+let email = "";
+let alerts = false;
+let date = "";
+
+function updatevar(type){
+    if(type == "license"){
+        drivingLicense = document.getElementById("license").value;
+        console.log(drivingLicense);
+    }
+    if(type == "postcode"){
+        postcode = document.getElementById("postcode").value;
+        console.log(postcode);
+    }
+    if(type == "email"){
+        email = document.getElementById("email").value;
+        console.log(email);
+    }
+    if(type == "alerts"){
+        if(document.getElementById("alerts").value == "True" || String(document.getElementById("alerts").value).toLowerCase() == "true"){
+            alerts = true;
+        }
+        else{
+            alerts = false;
+        }
+        console.log(alerts);
+    }
+    if(type == "date"){
+        date = document.getElementById("date").value;
+        console.log(date);
+    }
+}
+
+function saveVar(){
+    document.getElementById("success").classList.add("hidden");
+    document.getElementById("success").innerHTML = ""; 
+    if(drivingLicense.length > 14){
+        if(postcode.length > 3){
+            if(email.includes("@") && email.includes(".")){
+                if(date.includes("-") && date.length > 3){
+                    const curDate = new Date().setHours(0, 0, 0, 0);
+                    if(curDate < new Date(date)){
+                        document.getElementById("error").classList.add("hidden");
+                        document.getElementById("error").innerHTML = ""; 
+                        document.getElementById("success").classList.remove("hidden");
+                        document.getElementById("success").innerHTML = "Proceeding to save..."; 
+
+                        eel.saveScriptSettings(drivingLicense, postcode, email, alerts, date)
+                    }
+                    else{
+                        document.getElementById("error").classList.remove("hidden");
+                        document.getElementById("error").innerHTML = "Please select a date ahead of time!";                         
+                    }
+                }
+                else{
+                    document.getElementById("error").classList.remove("hidden");
+                    document.getElementById("error").innerHTML = "Please select a date!";                    
+                }
+            }
+            else{
+                document.getElementById("error").classList.remove("hidden");
+                document.getElementById("error").innerHTML = "Please enter a correct email!";
+            }
+        }
+        else{
+            document.getElementById("error").classList.remove("hidden");
+            document.getElementById("error").innerHTML = "Please enter a postcode!";
+        }
+    }
+    else{
+        document.getElementById("error").classList.remove("hidden");
+        document.getElementById("error").innerHTML = "Please enter the correct license number!";
+    }
+}
+
+eel.expose(returnSaveUpdate);
+function returnSaveUpdate(value){
+    if(value.toLowerCase() == "true"){
+        document.getElementById("error").classList.add("hidden");
+        document.getElementById("error").innerHTML = "";
+        document.getElementById("success").classList.remove("hidden");
+        document.getElementById("success").innerHTML = "File saved successfully!"; 
+    }
+    else{
+        document.getElementById("error").classList.remove("hidden");
+        document.getElementById("error").innerHTML = "Failed to save file! Try again.";
+        document.getElementById("success").classList.add("hidden");
+        document.getElementById("success").innerHTML = ""; 
+    }
+}
+
+eel.expose(returnLoadSaveUpdate);
+function returnLoadSaveUpdate(value){
+    if(value.toLowerCase() == "true"){
+        document.getElementById("error").classList.add("hidden");
+        document.getElementById("error").innerHTML = "";
+        document.getElementById("success").classList.remove("hidden");
+        document.getElementById("success").innerHTML = "Save loaded successfully!"; 
+    }
+    else{
+        document.getElementById("error").classList.remove("hidden");
+        document.getElementById("error").innerHTML = "Save is corrupt/incorrect!";
+        document.getElementById("success").classList.add("hidden");
+        document.getElementById("success").innerHTML = ""; 
+    }
+}
+
+eel.expose(loadScriptSave)
+function loadScriptSave(){
+
+}
+
+function loadSave(){
+    eel.loadScriptSettings("show");
+}
+
+eel.expose(updateNewSettings);
+function updateNewSettings(var1, var2, var3, var4, var5){
+    drivingLicense = var1;
+    postcode = var2;
+    email = var3;
+    alerts = var4;
+    date = var5;
+    console.log(drivingLicense + "\n" + postcode + "\n" + email + "\n" + alerts + "\n" + date);
+
+    document.getElementById("license").value = drivingLicense;
+    document.getElementById("postcode").value = postcode;
+    document.getElementById("email").value = email;
+    document.getElementById("alerts").value = alerts;
+    document.getElementById("date").value = date;
 }
--- main.py
+++ main.py
@@ -1,8 +1,57 @@
 from time import *
 from math import *
 import eel
+import os
 
 eel.init("gui")
+
+
+@eel.expose
+def saveScriptSettings(drivingLicense, postcode, email, alerts, date):
+    layout = f"Driving-License: {drivingLicense}\nPostcode: {postcode}\nEmail: {email}\nAlerts: {alerts}\nDate: {date}"
+    try:
+        with open("settings.ini", "w") as file:
+            file.write(layout)
+            print("Saved to system successfully!")
+            eel.returnSaveUpdate("true")
+    except:
+        print("Failed to save to system!")
+        eel.returnSaveUpdate("false")
+    
+@eel.expose
+def loadScriptSettings(style):
+    if not os.path.exists("settings.ini"):
+        print(f"Save file doesn't exist!")
+    else:
+        values = []
+        keys = []
+        with open("settings.ini", "r") as file:
+            for items in file:
+                if ":" in items:
+                    key, value = items.split(":", 1)
+                    values.append(value.strip())
+                    keys.append(key.strip())
+                    
+        hasPassedCheck = False
+        if "driving-license" in str(keys[0]).lower() and len(values[0]) > 14:
+            if "postcode" in str(keys[1]).lower() and len(values[1]) > 3:
+                if "email" in str(keys[2]).lower() and len(values[2]) > 4 and "@" in values[2] and "." in values[2]:
+                    if "alerts" in str(keys[3]).lower() and len(values[3]) > 3:
+                        if "date" in str(keys[4]).lower() and len(values[4]) > 1:
+                            hasPassedCheck = True
+        
+        if hasPassedCheck == True:
+            if style == "show":
+                eel.returnLoadSaveUpdate("true")
+            eel.updateNewSettings(values[0], values[1], values[2], values[3], values[4])
+        else:
+            if style == "hidden":
+                eel.returnLoadSaveUpdate("false")
+            print("Corrupt/Incorrect save file detected!")
+    
+@eel.expose
+def saveAppSettings():
+    print("")
 
 
 eel.start("index.html", size=(1080,720))
